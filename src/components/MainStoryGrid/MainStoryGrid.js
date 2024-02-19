57207979005c3ed802b4877b1e76c19fa8f094d6
Vaughn Walker <vaughn.walker@integral.io>
2024-02-19 12:56:31 -0500

    Add line clamp and dividers

--- src/components/MainStoryGrid/MainStoryGrid.js
+++ src/components/MainStoryGrid/MainStoryGrid.js
@@ -1,79 +1,88 @@
 import React from 'react';
 import styled from 'styled-components/macro';
 
-import {
-  MAIN_STORY,
-  OPINION_STORIES,
-  SECONDARY_STORIES,
-} from '../../data';
+import {MAIN_STORY, OPINION_STORIES, SECONDARY_STORIES,} from '../../data';
 
 import SectionTitle from '../SectionTitle';
 import MainStory from '../MainStory';
 import SecondaryStory from '../SecondaryStory';
 import OpinionStory from '../OpinionStory';
 import Advertisement from '../Advertisement';
+import {COLORS} from "../../constants";
 
 const MainStoryGrid = () => {
-  return (
-    <Wrapper>
-      <MainStorySection>
-        <MainStory {...MAIN_STORY} />
-      </MainStorySection>
+	return (
+		<Wrapper>
+			<MainStorySection>
+				<MainStory {...MAIN_STORY} />
+			</MainStorySection>
 
-      <SecondaryStorySection>
-        <StoryList>
-          {SECONDARY_STORIES.map((story, index) => (
-            <SecondaryStory key={story.id} {...story} />
-          ))}
-        </StoryList>
-      </SecondaryStorySection>
+			<SecondaryStorySection>
+				<StoryList>
+					{SECONDARY_STORIES.map((story, index) => (
+						<VerticalStoryWrapper key={story.id}>
+							<SecondaryStory {...story} />
+						</VerticalStoryWrapper>
+					))}
+				</StoryList>
+			</SecondaryStorySection>
 
-      <OpinionSection>
-        <SectionTitle>Opinion</SectionTitle>
-        <StoryList>
-          {OPINION_STORIES.map((story, index) => (
-            <OpinionStory key={story.id} {...story} />
-          ))}
-        </StoryList>
-      </OpinionSection>
+			<OpinionSection>
+				<SectionTitle>Opinion</SectionTitle>
+				<StoryList>
+					{OPINION_STORIES.map((story, index) => (
+						<VerticalStoryWrapper key={story.id}>
+							<OpinionStory{...story} />
+						</VerticalStoryWrapper>
+					))}
+				</StoryList>
+			</OpinionSection>
 
-      <AdvertisementSection>
-        <Advertisement />
-      </AdvertisementSection>
-    </Wrapper>
-  );
+			<AdvertisementSection>
+				<Advertisement/>
+			</AdvertisementSection>
+		</Wrapper>
+	);
 };
 
 const Wrapper = styled.div`
-  display: grid;
-  grid-template-areas:
+    display: grid;
+    grid-template-areas:
     'main-story'
     'secondary-stories'
     'opinion-stories'
     'advertisement';
-  gap: 48px;
-  margin-bottom: 48px;
+    gap: 48px;
+    margin-bottom: 48px;
 `;
 
 const MainStorySection = styled.section`
-  grid-area: main-story;
+    grid-area: main-story;
 `;
 
+const VerticalStoryWrapper = styled.div`
+    &:not(:last-of-type) {
+        border-bottom: 1px solid ${COLORS.gray[300]};
+        padding-bottom: 16px;
+        margin-bottom: 16px;
+    }
+`
+
 const SecondaryStorySection = styled.section`
-  grid-area: secondary-stories;
+    grid-area: secondary-stories;
 `;
 
 const StoryList = styled.div`
-  display: flex;
-  flex-direction: column;
+    display: flex;
+    flex-direction: column;
 `;
 
 const OpinionSection = styled.section`
-  grid-area: opinion-stories;
+    grid-area: opinion-stories;
 `;
 
 const AdvertisementSection = styled.section`
-  grid-area: advertisement;
+    grid-area: advertisement;
 `;
 
 export default MainStoryGrid;
